a3f806a207c9bed7a87c9ec7f5a1782fd6a8db0b
Amaury Rodriguez <amaury.rodriguez@gmail.com>
2015-10-27 00:22:44 -0400

    Populate station dropdowns

--- src/septa_challenge/timetable/views.py
+++ src/septa_challenge/timetable/views.py
@@ -2,6 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from . import models
+from . import services
 
 def home(request):
-    return render(request, 'main.html', {})
+    stations = models.Station.objects.all().order_by('name')
+
+    context = {'stations': stations}
+
+    return render(request, 'main.html', context)
+
+
+
+
+
